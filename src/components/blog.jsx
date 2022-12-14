7ae1f3cd5cc6d09ad388cc1f80f7a9d1cb25b5c8
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-14 14:31:41 -0800

    add isPublished state

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Blog(props) {
+    const [isPublished, setIsPublished] = useState(props.isPublished);
+
+    const handleClick = () => {
+        setIsPublished(prevState => !prevState);
+    }
+    
     return (
         <div className="blog padding-all-1" onClick={props.handleClick}>
             <h3>{props.title}</h3>
             <p>{props.body}</p>
             <p>{props.timestamp}</p>
+            <button onClick={handleClick}>{isPublished ? "Published" : "Not Published"}</button>
         </div>
     )
 }
