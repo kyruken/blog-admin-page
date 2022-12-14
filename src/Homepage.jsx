559f79301ee1ea2190c9cd3da9f0925d96ed1a5f
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-14 14:32:13 -0800

    add form for new blogs

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,34 +3,39 @@ import { useState, useEffect } from 'react';
 
 import './App.css';
 
-function Homepage() {
+function Homepage(props) {
     const [blogs, setBlogs] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/posts')
+            .then(response => response.json())
+            .then(data => setBlogs(data.posts));
 
-  useEffect(() => {
-    fetch('http://localhost:3000/posts')
-      .then(response => response.json())
-      .then(data => setBlogs(data.posts));
+    }, [])
 
-  }, [])
-
-  const blogElements = blogs.map(blog => {
-    return <Blog
-        body={blog.body}
-        comments={blog.comments}
-        isPublished={blog.isPublished}
-        title={blog.title}
-        key={blog._id}
-        id={blog._id}
+    const blogElements = blogs.map(blog => {
+        return <Blog
+            body={blog.body}
+            comments={blog.comments}
+            isPublished={blog.isPublished}
+            title={blog.title}
+            key={blog._id}
+            id={blog._id}
         >
-      </Blog>
-  })
+        </Blog>
+    })
 
-  return (
-    <div className="App">
-      <h1>Ibrablogs Admin Dashboard</h1>
-      <div className="grid grid-center blog-container padding-lr-5 padding-tb-1">{blogElements}</div>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>Ibrablogs Admin Dashboard</h1>
+            <div>
+                <input type='text' name='title' placeholder='Title'/>
+                <input type='text' name='body' placeholder='Body' />
+                <button>Submit Post</button>
+            </div>
+            <div className="grid grid-center blog-container padding-lr-5 padding-tb-1">{blogElements}</div>
+        </div>
+    )
 }
 
 export default Homepage
