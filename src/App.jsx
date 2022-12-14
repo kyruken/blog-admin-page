8ac6eb34846ee55b629076a91771a1d2dbb7ceb5
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-14 13:26:45 -0800

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-
 import './App.css';
 
 function App() {
+  //Set up user login at App level
+  //Once authenticated, render Homepage
 
   return (
     <div className="App">
