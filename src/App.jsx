dbe9134e344fcff0bfb52510dddf6707ab96ede5
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-14 18:25:22 -0800

    refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,16 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import axios from 'axios';
-
-import Homepage from './Homepage';
-
+import Login from './Login';
+import Blogpage from './Blogpage';
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState(null);
-  
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  //Set up user login at App level
-  //Once authenticated, render Homepage
-
-  useEffect(() => {
-    if (token) {
-      setLoginSuccess(true);
-    }
-
-  }, [token])
-
-  const logIn = () => {
-    axios({
-      method: "POST",
-      data: {
-        username: username,
-        password: password,
-      },
-      url: 'http://localhost:3000/api/login'
-    }).then(data => setToken(data.data.token));
-  }
-  console.log(token);
   return (
-    <div className="App">
-      {!loginSuccess && 
-      <div>
-        <h1>Login</h1>
-        <input type='text' name='username' placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-        <input type='password' name='password' placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={logIn}>Log in</button>
-      </div>
-      }
-      {loginSuccess &&
-      <Homepage />
-      }
-      
-    </div>
+    <Router className="App">
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/:blogId' element={<Blogpage />}></Route>
+
+      </Routes>
+    </Router>
   )
 }
 
