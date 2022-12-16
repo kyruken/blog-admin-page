6244d279d1203d28da08bf6c89b7e74d390389e1
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-16 09:57:20 -0800

    add delete button functionality

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function Comment(props) {
+    const deleteComment = () => {
+        axios({
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer " + JSON.parse(localStorage.getItem("token"))
+            },
+            url: `http://localhost:3000/posts/${props.blogId}/comments/${props.id}`
+        }).then(res => console.log(res));
+    }
+    
     return (
         <div className='comment padding-all-2 margin-bottom-1'>
             <div className='flex-row jc-space-between padding-bottom-1'>
@@ -8,6 +19,10 @@ export default function Comment(props) {
                 <p>{props.timestamp}</p>
             </div>
             <p>{props.comment}</p>
+            <div>
+                <button onClick={deleteComment}>Delete</button>
+                <button>Edit</button>
+            </div>
         </div>
     )
 }
