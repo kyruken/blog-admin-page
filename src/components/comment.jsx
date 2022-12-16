6e588e578aee76ba6553d94c0919db5c083cf69d
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-16 10:01:22 -0800

    add delete functionality clientside

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 export default function Comment(props) {
+    const [isDeleted, setIsDeleted] = useState(false);
     const deleteComment = () => {
         axios({
             method: "DELETE",
@@ -9,20 +10,23 @@ export default function Comment(props) {
                 Authorization: "Bearer " + JSON.parse(localStorage.getItem("token"))
             },
             url: `http://localhost:3000/posts/${props.blogId}/comments/${props.id}`
-        }).then(res => console.log(res));
+        }).then(setIsDeleted(true));
     }
     
     return (
-        <div className='comment padding-all-2 margin-bottom-1'>
-            <div className='flex-row jc-space-between padding-bottom-1'>
-                <p className='bold font-size-2'>{props.username}</p>
-                <p>{props.timestamp}</p>
-            </div>
-            <p>{props.comment}</p>
-            <div>
-                <button onClick={deleteComment}>Delete</button>
-                <button>Edit</button>
-            </div>
+        <div>
+            {!isDeleted &&
+            <div className='comment padding-all-2 margin-bottom-1'>
+                <div className='flex-row jc-space-between padding-bottom-1'>
+                    <p className='bold font-size-2'>{props.username}</p>
+                    <p>{props.timestamp}</p>
+                </div>
+                <p>{props.comment}</p>
+                <div>
+                    <button onClick={deleteComment}>Delete</button>
+                    <button>Edit</button>
+                </div>
+            </div>}
         </div>
     )
 }
