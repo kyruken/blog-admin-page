e20ca77aa9f12dcdea6d18944f8e1fb683be9dd8
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-16 11:25:57 -0800

    publish button makes put request

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function Blog(props) {
     const [isPublished, setIsPublished] = useState(props.isPublished);
 
-    const handleClick = () => {
-        setIsPublished(prevState => !prevState);
-    }
-    
+    const publishBlog = (e) => {
+        //e.preventDefault stops the Link component's onClick event
+        e.preventDefault();
+        axios({
+            method: "PUT",
+            headers: {
+              Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token')),
+            },
+            data: {
+              isPublished: !isPublished,
+            },
+            url: `http://localhost:3000/posts/${props.id}`
+          }).then(setIsPublished(prevState => !prevState));
+        }
+
     return (
         <div className="blog padding-all-1">
             <h3>{props.title}</h3>
             <p>{props.body}</p>
             <p>{props.timestamp}</p>
-            <button onClick={handleClick}>{isPublished ? "Published" : "Not Published"}</button>
+            <button onClick={(e) => publishBlog(e)}>{isPublished ? "Published" : "Not Published"}</button>
         </div>
     )
 }
