b9c72c101237387556c20bf087c61523733122a5
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-17 08:33:50 -0800

    add edit blog functionality

--- src/Blogpage.jsx
+++ src/Blogpage.jsx
@@ -1,13 +1,17 @@
-import { useState, useEffect }from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import Comment from './components/comment';
 export default function Blogpage() {
-    
+
     //useParams allows us to grab url params
     const id = useParams().blogId;
     const [blog, setBlog] = useState([]);
     const [comments, setComments] = useState([]);
+
+    const [isEdit, setIsEdit] = useState(false);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
     const [errorMsg, setErrorMsg] = useState(false);
     const [successMsg, setSuccessMsg] = useState(false);
@@ -16,9 +20,9 @@ export default function Blogpage() {
     useEffect(() => {
         Promise.all([
             fetch(`http://localhost:3000/posts/${id}`)
-            .then(res => res.json()),
+                .then(res => res.json()),
             fetch(`http://localhost:3000/posts/${id}/comments`)
-            .then(res => res.json())
+                .then(res => res.json())
         ]).then(data => {
             setBlog(data[0].post);
             setComments(data[1].comments.reverse());
@@ -37,9 +41,33 @@ export default function Blogpage() {
             setSuccessMsg(true);
         }, (err) => setErrorMsg(true))
     }
-    
+
+    const startEdit = () => {
+        setIsEdit(prevState => !prevState);
+    }
+    const sendEditBlog = () => {
+        axios({
+            method: "PUT",
+            headers: {
+                Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+            },
+            data: {
+                title: title,
+                description: description
+            },
+            url: `http://localhost:3000/posts/${id}`
+        }).then(res => {
+            setIsEdit(false);
+            setBlog(prevState => {
+                prevState.title = title;
+                prevState.body = description;
+                return prevState;
+            })
+        }, (err) => console.log(err));
+    }
+
     const commentElements = comments.map(comment => {
-        return <Comment 
+        return <Comment
             comment={comment.comment}
             username={comment.username}
             timestamp={comment.timestamp}
@@ -52,6 +80,12 @@ export default function Blogpage() {
     return (
         <div>
             <Link to='/'>Back</Link>
+            <button onClick={startEdit}>Edit contents</button>
+            {isEdit && <div>
+                <input type='text' name='title' placeholder='Title' onChange={(e) => setTitle(e.target.value)} />
+                <input type='text' name='description' placeholder='Body' onChange={(e) => setDescription(e.target.value)} />
+                <button onClick={sendEditBlog}>Submit Edit</button>
+            </div>}
             <div className='flex-column flex-center'>
                 {errorMsg && <h2>Server Error</h2>}
                 {successMsg && <h2>Successfully deleted</h2>}
